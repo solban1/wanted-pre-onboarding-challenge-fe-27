e61c2874570df544781d96cf5aa4336f5574f040
solban1 <solban1@daum.net>
2024-10-30 13:39:35 +0900

    Add eslint-plugin-react

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from "@eslint/js";
 import globals from "globals";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
+import react from "eslint-plugin-react";
 import tseslint from "typescript-eslint";
 import eslintConfigPrettier from "eslint-config-prettier";
 
@@ -9,6 +10,7 @@ export default tseslint.config(
   { ignores: ["dist"] },
   {
     name: "src",
+    settings: { react: { version: "detect" } },
     extends: [
       js.configs.recommended,
       ...tseslint.configs.strictTypeChecked,
@@ -27,13 +29,14 @@ export default tseslint.config(
     plugins: {
       "react-hooks": reactHooks,
       "react-refresh": reactRefresh,
+      react,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      "react-refresh/only-export-components": [
-        "warn",
-        { allowConstantExport: true },
-      ],
+      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      ...react.configs.recommended.rules,
+      ...react.configs["jsx-runtime"].rules,
+      "@typescript-eslint/no-unused-vars": "warn",
     },
   },
 );
